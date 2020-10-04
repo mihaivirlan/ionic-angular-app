b40b5277cdfd6db023371b7ac32b90a671aae6ac
mihaivirlan <mihaivirlan93@gmail.com>
2020-10-04 14:23:49 +0300

    ignore key

--- src/app/shared/map-modal/map-modal.component.ts
+++ src/app/shared/map-modal/map-modal.component.ts
@@ -51,7 +51,7 @@ export class MapModalComponent implements OnInit, AfterViewInit {
     }
     return new Promise((resolve, reject) => {
       const script = document.createElement('script');
-      script.src = 'https://maps.googleapis.com/maps/api/js?key='
+      script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCxsLzQCxREFvyT9luuZMUxDmYtBXfchV8'
       script.async = true;
       script.defer = true;
       document.body.appendChild(script);
