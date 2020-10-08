902122a20a79dc5801c49e160009113f1d9bfc68
mihaivirlan <mihaivirlan93@gmail.com>
2020-10-08 21:42:03 +0300

    Testing the Location Feature

--- src/app/shared/pickers/location-picker/location-picker.component.ts
+++ src/app/shared/pickers/location-picker/location-picker.component.ts
@@ -56,14 +56,18 @@ export class LocationPickerComponent implements OnInit {
       this.showErrorAlert();
       return;
     }
+    this.isLoading = true;
     Plugins.Geolocation.getCurrentPosition()
       .then(geoPosition => {
         const coordinates: Coordinates = {
           lat: geoPosition.coords.latitude,
           lng: geoPosition.coords.longitude
         };
+        this.createPLace(coordinates.lat, coordinates.lng);
+        this.isLoading = false;
       })
       .catch(err => {
+        this.isLoading = true;
         this.showErrorAlert();
     })
   }
@@ -71,7 +75,8 @@ export class LocationPickerComponent implements OnInit {
   private showErrorAlert() {
     this.alertCtrl.create({
       header: 'Could not fetch location',
-      message: 'Please use the map to pick a location!' 
+      message: 'Please use the map to pick a location!' ,
+      buttons: ['Okay']
     }).then(alertEl => alertEl.present());
   }
 
@@ -81,14 +86,25 @@ export class LocationPickerComponent implements OnInit {
         if (!modalData.data) {
           return;
         }
-        const pickedLocation: PlaceLocation = {
+        const coordinates: Coordinates = {
           lat: modalData.data.lat,
-          lng: modalData.data.lng,
-          address: null,
-          staticMapImageUrl: null
+          lng: modalData.data.lng
         };
-        this.isLoading = true;
-        this.getAddress(modalData.data.lat, modalData.data.lng).pipe(
+        this.createPLace(coordinates.lat, coordinates.lng);
+      });
+      modalEl.present();
+    });
+  }
+
+  private createPLace(lat: number, lng: number) {
+    const pickedLocation: PlaceLocation = {
+      lat: lat,
+      lng: lng,
+      address: null,
+      staticMapImageUrl: null
+    };
+    this.isLoading = true;
+        this.getAddress(lat, lng).pipe(
           switchMap(address => {
             pickedLocation.address = address;
             return of(this.getMapImage(pickedLocation.lat, pickedLocation.lng, 14));
@@ -99,9 +115,6 @@ export class LocationPickerComponent implements OnInit {
           this.isLoading = false;
           this.locationPick.emit(pickedLocation);
         });
-      });
-      modalEl.present();
-    });
   }
 
   private getAddress(lat: number, lng: number) {
