b08ee1c307ba2a9dc5aa137b4d31a776fa023e37
mihaivirlan <mihaivirlan93@gmail.com>
2020-10-09 15:47:21 +0300

    Converting the Image String to a File

--- src/app/shared/pickers/image-picker/image-picker.component.ts
+++ src/app/shared/pickers/image-picker/image-picker.component.ts
@@ -9,7 +9,7 @@ import { Platform } from '@ionic/angular';
 })
 export class ImagePickerComponent implements OnInit {
   @ViewChild('filePicker') filePickerRef: ElementRef<HTMLInputElement>;
-  @Output() imagePick = new EventEmitter<string>();
+  @Output() imagePick = new EventEmitter<string | File>();
   selectedImage: string;
   usePicker = false;
 
@@ -56,6 +56,7 @@ export class ImagePickerComponent implements OnInit {
     fr.onload = () => {
       const dataUrl = fr.result.toString();
       this.selectedImage = dataUrl;
+      this.imagePick.emit(pickedFile);
     };
     fr.readAsDataURL(pickedFile);
   }
