4c8eb724f5182d9fb99785ad05ada3b9ca13d3f7
mihaivirlan <mihaivirlan93@gmail.com>
2020-10-12 16:02:56 +0300

    Fixing the Subscription

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MenuController } from '@ionic/angular';
 import { SegmentChangeEventDetail } from '@ionic/core';
 import { Subscription } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 import { PlacesService } from '../places.service';
 import { Place } from '../place.model';
@@ -45,7 +46,7 @@ export class DiscoverPage implements OnInit, OnDestroy {
   }
 
   onFilterUpdate(event: CustomEvent<SegmentChangeEventDetail>) {
-    this.authService.userId.subscribe(userId => {
+    this.authService.userId.pipe(take(1)).subscribe(userId => {
       if (event.detail.value === 'all') {
         this.relevantPlaces = this.loadedPlaces;
         this.listedLoadedPlaces = this.relevantPlaces.slice(1);
