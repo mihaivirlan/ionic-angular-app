20611d3b19d5e575f5a6bb9492bbc49b668ea87b
mihaivirlan <mihaivirlan93@gmail.com>
2020-10-12 13:19:43 +0300

    Refactoring the Authentication Code

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
-import { LoadingController } from '@ionic/angular';
+import { AlertController, LoadingController } from '@ionic/angular';
 
 import { AuthService } from './auth.service';
 
@@ -17,23 +17,35 @@ export class AuthPage implements OnInit {
   constructor(
     private authService: AuthService,
     private router: Router,
-    private loadingCtrl: LoadingController
+    private loadingCtrl: LoadingController,
+    private alertCtrl: AlertController
   ) {}
 
   ngOnInit() {}
 
-  onLogin() {
+  authenticate(email: string, password: string) {
     this.isLoading = true;
     this.authService.login();
     this.loadingCtrl
       .create({ keyboardClose: true, message: 'Logging in...' })
       .then(loadingEl => {
         loadingEl.present();
-        setTimeout(() => {
+        this.authService.signup(email, password)
+        .subscribe(resData => {
+          console.log(resData);
           this.isLoading = false;
           loadingEl.dismiss();
           this.router.navigateByUrl('/places/tabs/discover');
-        }, 1500);
+        }, errRes => {
+          loadingEl.dismiss();
+            const code = errRes.error.error.message;
+            let message = 'Could not sign you up, please try again.'
+            if (code === 'EMAIL_EXISTS') {
+              message = 'This email address exists already!';
+            }
+            this.showAlert(message);
+          }
+        );
       });
   }
 
@@ -48,14 +60,13 @@ export class AuthPage implements OnInit {
     const email = form.value.email;
     const password = form.value.password;
 
-    if (this.isLogin) {
-      // Send a request to login servers
-    } else {
-      // Send a request to signup servers
-      this.authService.signup(email, password)
-        .subscribe(resData => {
-          console.log(resData);
-        });
-    }
+    this.authenticate(email, password);
+  }
+
+  private showAlert(message: string) {
+    this.alertCtrl.create(
+      { header:'Authentication failed', message: message, buttons: ['Okay'] }
+    )
+    .then(alertEl => alertEl.present());
   }
 }
