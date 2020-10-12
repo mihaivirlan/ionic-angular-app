07735da1e6edbc3b08b7b3d014985424321e3f9e
mihaivirlan <mihaivirlan93@gmail.com>
2020-10-12 14:24:53 +0300

    Managing the User with a Subject

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { user } from 'firebase-functions/lib/providers/auth';
+import { BehaviorSubject } from 'rxjs';
+import { map } from 'rxjs/operators';
+
 import { environment } from '../../environments/environment';
+import { User } from './user.model';
 
 export interface AuthResponseData {
   kind: string;
@@ -16,15 +21,30 @@ export interface AuthResponseData {
   providedIn: 'root'
 })
 export class AuthService {
-  private _userIsAuthenticated = false;
-  private _userId = null;
+  private _user = new BehaviorSubject<User>(null);
 
   get userIsAuthenticated() {
-    return this._userIsAuthenticated;
+    return this._user.asObservable().pipe(
+      map(user => {
+        if (user) {
+          return !!user.token;
+        } else {
+          return false;
+        }
+      })
+    );
   }
 
   get userId() {
-    return this._userId;
+    return this._user.asObservable().pipe(
+      map(user => {
+        if (user) {
+          return user.id;
+        } else {
+          return null;
+        }
+      })
+    );
   }
 
   constructor(private http: HttpClient) {}
@@ -46,6 +66,6 @@ export class AuthService {
   }
 
   logout() {
-    this._userIsAuthenticated = false;
+    this._user.next(null);
   }
 }
