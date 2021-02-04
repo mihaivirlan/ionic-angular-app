c736c334d11e3598723809f1410967e47c91d5bb
mihaivirlan <mihaivirlan93@gmail.com>
2021-02-04 19:17:12 +0200

    second commit

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM node:10-alpine
+
+COPY ./ /home/node/app
+
+WORKDIR /home/node/app
+
+RUN npm install
+
+EXPOSE 8080
+
+CMD [ "ng", "serve" ]
---
+++ src/app/app.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Platform } from '@ionic/angular';
+import { Plugins, Capacitor } from '@capacitor/core';
+
+import { AuthService } from './auth/auth.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html'
+})
+export class AppComponent implements OnInit, OnDestroy {
+  private authSub: Subscription;
+  private previousAuthState = false;
+
+  constructor(
+    private platform: Platform,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      if (Capacitor.isPluginAvailable('SpashScreen')) {
+        Plugins.SplashScreen.hide();
+      }
+    });
+  }
+
+  ngOnInit() {
+    this.authSub = this.authService.userIsAuthenticated
+    .subscribe(isAuth => {
+      if (!isAuth && this.previousAuthState !== isAuth) {
+        this.router.navigateByUrl('/auth');
+      }
+      this.previousAuthState = isAuth;
+    });
+  }
+
+  onLogout() {
+    this.authService.logout();
+  }
+
+  ngOnDestroy() {
+    if (this.authSub) {
+      this.authSub.unsubscribe();
+    }
+  }
+}
